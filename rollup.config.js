4dae1194370a1d76b47659edce81baa2177085da
iRace-it <bijacky@163.com>
2022-05-29 01:08:45 +0800

    export button

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
-import { babel } from '@rollup/plugin-babel'
 import dts from 'rollup-plugin-dts'
 import commonjs from '@rollup/plugin-commonjs'
 import pkg from './package.json'
@@ -15,7 +14,6 @@ export default [
         sourcemap: true,
       },
     ],
-    external: [...Object.keys(pkg.peerDependencies || {})],
     plugins: [commonjs(), nodeResolve(), typescript(/* { plugin options } */)],
   },
   {
